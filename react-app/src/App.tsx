b785795d6e41517ee4d96baa57d40f5b9bec897a
Evan Kline Mores <21-03567@g.batstate-u.edu.ph>
2024-12-21 09:27:35 +0800

    Passing children

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  }
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectedIndex={handleSelectedItem}/>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
